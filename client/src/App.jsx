06b07fcada48efd68226db88ce555fa25e73e290
Harsh Arya <harsharya8946@gmail.com>
2026-08-11 21:33:19 +0530

    redux setup, auth api's & useAuth hook

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { RouterProvider } from 'react-router'
 import { routes } from './App.router'
+import { store } from './App.store'
+import { Provider } from '@reduxjs/toolkit'
 
 const App = () => {
   return (
-   <RouterProvider router={routes} />
+    <Provider store={store} >
+      <RouterProvider router={routes} />
+    </Provider>
   )
 }
 
---
+++ client/src/features/auth/hook/useAuth.js
@@ -0,0 +1,54 @@
+import { useDispatch } from '@reduxjs/toolkit'
+import { register, login, getme } from '../services/auth.api'
+import { setUser, setLoading, setError } from '../auth.slice'
+
+export function useAuth(){
+
+    const dispatch = useDispatch()
+
+    async function registerController({ username, email, password }){
+       try {
+            dispatch(setLoading(true))
+            const data = await register({ username, email, password })
+            dispatch(setUser(data))
+       } 
+       catch (error) {
+            dispatch(setError(error))
+       } 
+       finally {
+            dispatch(setLoading(false))
+       }
+    }
+
+
+    async function loginController({ email, password }) {
+        try{
+            dispatch(setLoading(true))
+            const data = await login({ email, password })
+            dispatch(setUser(data))
+        }
+        catch (error) {
+            dispatch(setError(error))
+        }
+        finally {
+            dispatch(setLoading(false))
+        }
+    }
+
+    async function getmeController(){
+        try {
+            dispatch(setLoading(true))
+            const data = await getme()
+            dispatch(setUser(data))
+        }
+        catch (error) {
+            dispatch(setError(error))
+        }
+        finally {
+            dispatch(setLoading(false))
+        }
+    }
+
+    return { registerController, loginController, getmeController }
+
+}
