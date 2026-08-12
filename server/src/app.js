531b0c8a46be9379b1608075cacf9187c69cb792
Harsh Arya <harsharya8946@gmail.com>
2026-08-12 23:41:18 +0530

    cors setup

--- server/src/app.js
+++ server/src/app.js
@@ -2,11 +2,16 @@ import express from 'express'
 import authRouter from './route/auth.route.js'
 import cookieParser from 'cookie-parser'
 import blogRouter from './route/blog.route.js'
+import cors from 'cors'
 
 const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors({
+    origin : "http://localhost:5173",
+    credentials : true
+}))
 
 app.use('/api/auth', authRouter)
 app.use('/api/blog', blogRouter)
